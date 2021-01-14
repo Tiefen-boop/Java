2270cb3bf2b07b637bb15e48656fb1fe5890d904
Tiefen-boop <roi.tief97@gmail.com>
2021-01-14 22:20:58 +0200

    Bubble Sort

---
+++ SortingOutMyProblems/src/BubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {6,2,7,28,8,32,7,65,4,25432,76,5,1,2,54};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j< arr.length - i - 1; j++)
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] =tmp;
+                }
+    }
+}
