a9e6eb1dc0ec12c0700602a68a7aa77048b19429
Tiefen-boop <roi.tief97@gmail.com>
2021-01-20 19:09:45 +0200

    Fib update

--- RecursionGames/src/Fibonacci.java
+++ RecursionGames/src/Fibonacci.java
@@ -1,25 +1,34 @@
 public class Fibonacci {
     public static void main(String[] args) {
+        for ( int i = 0; i< 10; i++)
+        System.out.print(fibCount(0, i) + ", ");
     }
 
-    public static int fibIterative(int n){
-        if (n==0 | n==1)
+    public static int fibIterative(int n) {
+        if (n < 2)
             return 1;
-        int pos = 2, posMin1 = 1, posMin2 = 1;
-        n -=2;
-        while (n>=0){
-            pos = posMin1 + posMin2;
-            posMin2 = posMin1;
-            posMin1 = pos;
-            n--;
+        int ans = 2, prev = 1;
+        for (int i = 2; i <= n; i++) {
+            ans += prev;
+            prev = ans - prev;
         }
-        return pos;
+        return ans;
     }
 
     public static int fibRecursive(int n){
         if (n==0 | n==1)
             return 1;
         return fibRecursive(n-1) + fibRecursive(n-2);
+    }
+
+    public static int fibCount(int m, int n){ //counts the number of calls to fibRecursive(m) when calculating fibRecursive(n)
+        if (m > n)
+            return 0;
+        if (m == n)
+            return 1;
+        if (n < 2)
+            return 0;
+        return fibCount(m , n-1) + fibCount(m , n-2);
     }
 
     public static int fibRecMemoized(int n){
